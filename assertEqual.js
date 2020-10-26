4fddf520acc904205dc4b43c4d9961eafa6b06ab
Kevinli296 <kevinli296@outlook.com>
2020-10-26 21:18:27 +0000

    touching up the code

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,11 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('ASSERTION PASSED✔️✔️✔️: ' + actual + ' === ' + expected);
+    console.log(`ASSERTION PASSED✅✅✅: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log('ASSERTION FAILED❌❌❌: ' + actual + ' !== ' + expected);
+    console.log(`ASSERTION FAILED❌❌❌: ${actual} !== ${expected}`);
   }
 };
-
+  
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
