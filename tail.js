6debf966b9e064da88cf667e211d17b5ecbb7b79
Kevinli296 <kevinli296@outlook.com>
2020-10-26 21:45:32 +0000

    created tail function that prints every elementin the array except the first

---
+++ tail.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('ASSERTION PASSED✔️✔️✔️: ' + actual + ' === ' + expected);
+  } else if (actual !== expected) {
+    console.log('ASSERTION FAILED❌❌❌: ' + actual + ' !== ' + expected);
+  }
+};
+
+
+const tail = function(array) {
+  let newArr = [];
+  for (let i = 1; i < array.length; i++) {
+    newArr.push(array[i]);
+  }
+  return newArr;
+};
+
+
+// TEST CODE
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words)); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
