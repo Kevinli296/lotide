c39135b937393c08aa163a52b2d095e094c50478
Kevinli296 <kevinli296@outlook.com>
2020-10-26 21:13:08 +0000

    new function assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('ASSERTION PASSED✔️✔️✔️: ' + actual + ' === ' + expected);
+  } else if (actual !== expected) {
+    console.log('ASSERTION FAILED❌❌❌: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('hi', 'hi');
+assertEqual('hi', 'bye');
+assertEqual(3, 3);
+assertEqual(6, 10);
