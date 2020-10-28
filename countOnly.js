684ba6fb52b32582ea9215db025ba6f84c99f8c5
Kevinli296 <kevinli296@outlook.com>
2020-10-28 21:38:14 +0000

    learning how to count specific items

---
+++ countOnly.js
@@ -0,0 +1,67 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`ASSERTION PASSED✅✅✅: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`ASSERTION FAILED❌❌❌: ${actual} !== ${expected}`);
+  }
+};
+
+//this function should take in a collection of items
+//return counts for a specific subset of those items
+//it will NOT count everything
+//it will be given an idea of which items we care about and will ONLY count those
+//it will ignore the others
+//items in our case will be limited to STRINGS
+//countOnly will be given an array and an object
+//it will return an object containing counts of everything in the input object listed
+
+
+// start by defining the function
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+
+
+const countOnly = (allItems, itemsToCount) => {
+
+  const results = {};
+
+  for (item of allItems) {
+    if (itemsToCount[item]) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+}
+  return results;
+}
+
+
+
+const result1 = countOnly(firstNames, {
+  "Jason": true,
+  "Karima": true,
+  "Fang": true,
+  "Agouhanna": false
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+//the function should report back how many instances of each string were found in the allItems array
