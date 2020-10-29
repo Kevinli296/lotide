2bbfed174c136a374ce24234139fc9b4851d1b55
Kevinli296 <kevinli296@outlook.com>
2020-10-29 01:26:09 +0000

    function to count letters

--- countLetters.js
+++ countLetters.js
@@ -6,14 +6,6 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`ASSERTION PASSED✅✅✅: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`ASSERTION FAILED❌❌❌: ${actual} !== ${expected}`);
-  }
-};
-
 //create a function called countLetters
 //function should take in a sentence (as a string)
 //it should then return a count of each of the letters in that sentence
@@ -42,8 +34,21 @@ const countLetters = (string) => {
   return result;
 };
 
-console.log(countLetters('LHLHLHL'));
-console.log(countLetters('ABBCCCDDDDEEEEEFFFFFF'));
+
+
+const test1 = countLetters('LHLHLHL');
+const test2 = countLetters('ABBCCCDDDD');
+
+//TEST CODE
+assertEqual(test1['L'], 4);
+assertEqual(test1['H'], 3);
+assertEqual(test1['C'], undefined);
+assertEqual(test1['A'], undefined);
+assertEqual(test2['A'], 1);
+assertEqual(test2['B'], 2);
+assertEqual(test2['C'], 3);
+assertEqual(test2['D'], 4);
+assertEqual(test2['E'], undefined);
 
 // output => 
 // { L: 4,
