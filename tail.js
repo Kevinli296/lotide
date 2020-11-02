0cafcb7a90c1266c1d25f8cf04eed2c314e0ff55
Kevinli296 <kevinli296@outlook.com>
2020-11-02 22:02:10 +0000

    removed commented test cases

--- tail.js
+++ tail.js
@@ -9,8 +9,3 @@ const tail = function(array) {
 };
 
 module.exports = tail;
-
-// TEST CODE
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// console.log(tail(words)); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
