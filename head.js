e0bd9edfe5a52142128b6881445962f7842b68db
Kevinli296 <kevinli296@outlook.com>
2020-11-02 21:37:58 +0000

    deleted commented test cases

--- head.js
+++ head.js
@@ -11,8 +11,3 @@ const head = function(array) {
 
 module.exports = head;
 
-// TEST CODE
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([]), 6);
-// assertEqual(head([21]), 21);
