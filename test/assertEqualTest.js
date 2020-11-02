eb08fcd2984ec77bdd2ce342ba2db0eb4d9ca0b0
Kevinli296 <kevinli296@outlook.com>
2020-11-02 20:44:53 +0000

    test files for refactoring

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('hi', 'hi');
+assertEqual('hi', 'bye');
+assertEqual(3, 3);
+assertEqual(6, 10);
