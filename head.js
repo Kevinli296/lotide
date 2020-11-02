1d6c0ad3332b802378bc9fc05fe7ef7ed3406d47
Kevinli296 <kevinli296@outlook.com>
2020-11-02 20:57:15 +0000

    linting

--- head.js
+++ head.js
@@ -1,11 +1,3 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log('ASSERTION PASSED✅✅✅: ' + actual + ' === ' + expected);
-//   } else if (actual !== expected) {
-//     console.log('ASSERTION FAILED❌❌❌: ' + actual + ' !== ' + expected);
-//   }
-// };
-
 const assertEqual = require('./assertEqual');
 
 const head = function(array) {
